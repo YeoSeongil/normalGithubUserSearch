ac0abcfe5aca19628f8181e30f6f034b4766d59a
YeoSeongil <seongil_yeo@naver.com>
2024-02-06 18:03:36 +0900

    [CHORE] Singleton으로 APIManager 구현, 에러 처리를 위한 DataError Enum 구현, 여러 모델의 타입을 받아올 수 있도록 제네릭 함수 request 구현

--- normalGithubUserSearch/Helper/APIManager.swift
+++ normalGithubUserSearch/Helper/APIManager.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+enum DataError: Error {
+    case invalidResponse
+    case invalidURL
+    case invalidData
+    case network(Error?)
+}
+
+typealias Handler<T> = (Result<T, DataError>) -> Void
+
+final class APIManager {
+    
+    static let shared = APIManager()
+    private init() { }
+    
+    func request<T: Decodable>(
+        modelType: T.Type,
+        type: EndPointType,
+        completion: @escaping Handler<T>
+    ) {
+        guard let url = type.url else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data, error == nil else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  200 ... 299 ~= response.statusCode else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let data = try JSONDecoder().decode(modelType, from: data)
+                completion(.success(data))
+            }catch {
+                completion(.failure(.network(error)))
+            }
+        }.resume()
+    }
+}
