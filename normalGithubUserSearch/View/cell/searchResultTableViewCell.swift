56fc01ac52e8675f9e8cfebcd87d73449fc468e2
YeoSeongil <seongil_yeo@naver.com>
2024-02-06 18:05:30 +0900

    [CHORE] Cell 구현, 프로퍼티 옵저버 users 변수를 사용해서 모델 값의 변경이 있으면 자동으로 configuration 하도록 구현

--- normalGithubUserSearch/View/cell/searchResultTableViewCell.swift
+++ normalGithubUserSearch/View/cell/searchResultTableViewCell.swift
@@ -7,17 +7,72 @@
 
 import UIKit
 
-class searchResultTableViewCell: UITableViewCell {
+class searchResultCollectionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let id:String  = "searchResultCollectionViewCell"
+    
+    var users: Item? {
+        didSet {
+            configuration()
+        }
+    }
+    
+    private let userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 30
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .clear
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setView()
+        self.setConstaraint()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setView() {
+        [userImageView, nameLabel].forEach {
+            self.addSubview($0)
+        }
+        self.backgroundColor = .clear
+        self.layer.cornerRadius = CGFloat(5)
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    private func setConstaraint() {
+        userImageView.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        userImageView.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor, constant: -15).isActive = true
+        //userImageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        //userImageView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        userImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        userImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        nameLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        nameLabel.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -15).isActive = true
     }
 
+    func configuration() {
+        guard let users else { return }
+       //self.userImageView.image = UIImage(named: users.items[0].avatarURL)
+        self.userImageView.setImageKingfisher(with: users.avatarURL)
+        self.nameLabel.text = users.login
+    }
 }
