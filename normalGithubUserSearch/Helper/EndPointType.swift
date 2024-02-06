ed2184ba9cda331f8378690a79ce10fe8183ffd1
YeoSeongil <seongil_yeo@naver.com>
2024-02-06 18:04:25 +0900

    [CHORE] URL 관리 용이하게 하기 위해 EndPointType 프로토콜을 준수하는 EndPointItems 구현

--- normalGithubUserSearch/Helper/EndPointType.swift
+++ normalGithubUserSearch/Helper/EndPointType.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+enum HTTPMethods: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+protocol EndPointType {
+    var path: String { get }
+    var baseURL: String { get }
+    var url: URL? { get }
+    var method: HTTPMethods { get }
+}
+
+enum EndPointItems {
+    case initUsers
+    case Users(String)
+}
+
+extension EndPointItems: EndPointType {
+    var path: String {
+        switch self {
+        case .initUsers:
+            return "search/users?q=seongil"
+        case .Users(let query):
+            return "search/users?q=\(query)"
+        }
+    }
+    
+    var baseURL: String {
+        return "https://api.github.com/"
+    }
+    
+    var url: URL? {
+        return URL(string: "\(baseURL)\(path)")
+    }
+    
+    var method: HTTPMethods {
+        switch self {
+        case .initUsers:
+            return .get
+        case .Users:
+            return .get
+        }
+    }
+    
+    
+}
