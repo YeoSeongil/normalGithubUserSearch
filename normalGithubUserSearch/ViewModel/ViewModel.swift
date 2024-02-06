d65c6d12cad08c6a612b48a3e00879ecabbb68b5
YeoSeongil <seongil_yeo@naver.com>
2024-02-06 18:05:40 +0900

    [CHORE] fetchData, searchData 구현

--- normalGithubUserSearch/ViewModel/ViewModel.swift
+++ normalGithubUserSearch/ViewModel/ViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+class ViewModel {
+    
+    var users: [Users] = []
+    var item: [Item] = []
+    var eventHandler: ((_ event: Event) -> Void)?
+    
+    func fetchData() {
+        self.eventHandler?(.fetchData)
+        APIManager.shared.request(modelType: Users.self, type: EndPointItems.initUsers) {  result  in
+            switch result {
+            case .success(let users):
+                self.users = [users]
+                self.item = users.items
+                self.eventHandler?(.dataLoaded)
+            case.failure(let error):
+                self.eventHandler?(.fetchError(error))
+            }
+        }
+    }
+    
+    func serachData(_ inputSearchData: String) {
+        self.eventHandler?(.fetchData)
+        APIManager.shared.request(modelType: Users.self, type: EndPointItems.Users(inputSearchData)) { result in
+            switch result {
+            case .success(let users):
+                self.users = [users]
+                self.item = users.items
+                self.eventHandler?(.searchLoaded)
+            case.failure(let error):
+                self.eventHandler?(.fetchError(error))
+            }
+        }
+    }
+}
+
+extension ViewModel {
+    enum Event {
+        case fetchData
+        case dataLoaded
+        case fetchError(Error)
+        case searchLoaded
+    }
+}
